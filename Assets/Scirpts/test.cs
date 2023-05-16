d73b1a1b4d0127b26b4025b532b5b3611a980fd6
Felix850764001 <52335830+Felix850764001@users.noreply.github.com>
2023-05-16 12:35:53 +0800

    初始化提交

---
+++ Assets/Scirpts/test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("这是一个初始化代码文件");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
